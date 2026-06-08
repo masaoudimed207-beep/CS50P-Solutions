c234904762e9043bc1536b9b2aaa1612c1cbb8b5
Mohamed Messaoudi <masaoudimed207@gmail.com>
2026-06-08 16:02:09 +0000

    feat: implement figlet ASCII art generator with dynamic command-line font selection

---
+++ figlet/figlet.py
@@ -0,0 +1,18 @@
+import sys 
+import random
+from pyfiglet import Figlet
+figlet=Figlet()
+
+if len(sys.argv)==1:
+    font=random.choice(figlet.getFonts())
+    figlet.setFont(font=font)
+elif len(sys.argv)==3 and (sys.argv[1]=="-f" or sys.argv[1]=="--font") and sys.argv[2] in figlet.getFonts():
+    figlet.setFont(font=sys.argv[2])
+else:
+    sys.exit("Invalid usage")
+
+    
+text=input("Input: ")
+print(figlet.renderText(text))
+        
+        
